2aabcc4e8d5cefeb669adbce1784b7a4e4cc3780
Gabriel Boeira <gabriielboeira@hotmail.com>
2024-10-08 17:14:09 -0300

    politica-privacidade

--- Js/main.js
+++ Js/main.js
@@ -60,3 +60,24 @@ document.addEventListener('DOMContentLoaded', function() {
     const themeIcon = document.getElementById("theme-icon");*/
 
   });
+
+  // Espera a página carregar completamente
+window.onload = function() {
+  var popup = document.getElementById('popup');
+  var closeButton = document.getElementById('close-popup');
+
+  // Exibe o pop-up quando a página carrega
+  popup.style.display = 'flex';
+
+  // Fecha o pop-up ao clicar no botão "Entendi"
+  closeButton.onclick = function() {
+      popup.style.display = 'none';
+  };
+
+  // Fecha o pop-up ao clicar fora do conteúdo do pop-up
+  window.onclick = function(event) {
+      if (event.target == popup) {
+          popup.style.display = 'none';
+      }
+  };
+};

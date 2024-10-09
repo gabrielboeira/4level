9f5914bd2e1f1a771ffc10041db28d32a7ef2274
Gabriel Boeira <gabriielboeira@hotmail.com>
2024-10-08 23:17:39 -0300

    sendv2

--- Js/send.js
+++ Js/send.js
@@ -4,7 +4,7 @@ emailjs.init("TYYnUMx-zhFbCENlf");
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault(); // Previne o comportamento padrão do formulário
 
-    emailjs.sendForm("service_02u4nxt", "template_l51207q", this)
+    emailjs.sendForm("service_02u4nxt", "template_4rdaj7c", this)
         .then(function(response) {
             showMessage("Mensagem enviada com sucesso!", "success");
             console.log("Success:", response);
